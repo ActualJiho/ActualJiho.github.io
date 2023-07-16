ef478fa9477ef1a14dc4ee998d28bb725673e0b7
jiho-burgesskim <128258002+jiho-burgesskim@users.noreply.github.com>
2023-07-16 17:03:40 +1200

    Co-authored-by: ben-hoggan <ben-hoggan@users.noreply.github.com>

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/*.html'],
+  theme: {
+    extend: {
+      animation: {
+        blob: 'blob 7s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(30px, -50px) scale(1.8)',
+          },
+          '66%': {
+            transform: 'translate(-40px, 40px) scale(1.1)',
+          },
+          '100%': {
+            transform: 'tranlate(0px, 0px) scale(1)',
+          },
+          plugins: [],
+        },
+      },
+    },
+  },
+}
